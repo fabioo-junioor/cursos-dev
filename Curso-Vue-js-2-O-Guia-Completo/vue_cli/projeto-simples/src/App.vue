512b54d1e0d96687bcf8c19cac9dbf04c31907b4
FabioJr <fj_011@hotmail.com>
2022-02-27 21:14:24 -0300

    cursos realizados

---
+++ Curso-Vue-js-2-O-Guia-Completo/vue_cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Teste'
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    background-color: aquamarine;
+    color: brown;
+    font-size: 2rem;
+
+  }
+</style>

ccaa3d83b368c0111db86f3459db1d0e17611c6f
FabioJr <fj_011@hotmail.com>
2022-03-03 13:12:47 -0300

    cursos realizados

---
+++ Curso-Vue-js-2-O-Guia-Completo/componentes_avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot name="texto"></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+        text-align: center;
+    }
+    h1{
+        color: rgba(255, 0, 0, 0.70);
+    }
+</style>

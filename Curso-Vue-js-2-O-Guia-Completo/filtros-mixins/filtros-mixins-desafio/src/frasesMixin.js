2f0147a0f6ed0c8e75a564f850aa56c500886f99
FabioJr <fj_011@hotmail.com>
2022-03-07 19:01:37 -0300

    cursos realizados

---
+++ Curso-Vue-js-2-O-Guia-Completo/filtros-mixins/filtros-mixins-desafio/src/frasesMixin.js
@@ -0,0 +1,11 @@
+export default{
+    computed: {
+		fraseComVirgulas(){
+			return this.frase.replace(/\s/g, ',')
+
+		},
+		fraseComTamanho(){
+			return this.frase.split(' ').map(p => `${p} (${p.length})`).join(' ')
+		}
+	}
+}

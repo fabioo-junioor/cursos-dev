c00cd720c2e2bb8d91ec5f1b340745563c026767
FabioJr <fj_011@hotmail.com>
2022-03-22 21:02:06 -0300

    cursos realizados

---
+++ Curso-Vue-js-2-O-Guia-Completo/rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="usuario-detalhe">
+    <h3>Usuario Detalhe</h3>
+    <p><strong>Codigo: </strong>{{id}}</p>
+    <!--
+    <router-link primario 
+        tag="button" 
+        :to="`/usuario/${id}/editar`">Editar</router-link>
+    -->
+    <router-link primario
+        tag="button"
+        :to="{name: 'editarUsuario', params: {id},
+            query: {completo: true, lingua: 'pt'},
+            hash: '#rodape'}"
+        >Editar</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    beforeRouteEnter(to, from, next){
+        console.log('dentro do componente -> usuario detalhe')
+        next(vm => {
+            console.log(vm.id)
+        })
+    }
+    /*
+    data(){
+        return{
+            id: this.$route.params.id
+
+        }
+    },
+    watch: {
+        $route(to, from){
+            this.id = to.params.id
+
+        }
+    },
+    */
+}
+</script>
+
+<style>
+
+</style>

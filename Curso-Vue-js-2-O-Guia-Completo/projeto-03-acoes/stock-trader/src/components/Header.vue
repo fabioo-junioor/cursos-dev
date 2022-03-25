430fc7e91ff2476e570a152e461f7c06de2d8987
FabioJr <fj_011@hotmail.com>
2022-03-24 23:33:46 -0300

    cursos realizados

---
+++ Curso-Vue-js-2-O-Guia-Completo/projeto-03-acoes/stock-trader/src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+      <span>Stock</span>
+      <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat to="/">Inicio</v-btn>
+      <v-btn flat to="/portfolio">Portfolio</v-btn>
+      <v-btn flat to="/stocks">Ação</v-btn>
+    </v-toolbar-items>
+
+    <v-space></v-space>
+
+    <v-toolbar-items>
+      <v-btn flat>Finalizar Dia</v-btn>
+      <v-menu offset-y>
+        <b-btn flat slot="activator">Salvar e Carregar</b-btn>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title>Salvar Dados</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-title>Carregar Dados</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Curso-Vue-js-2-O-Guia-Completo/projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'bmw', price: 110},
+    {id: 2, name: 'google', price: 200},
+    {id: 3, name: 'apple', price: 250},
+    {id: 4, name: 'twitter', price: 80},
+]

1037c4522f6a90379ecda05691569e0017c798a7
FabioJr <fj_011@hotmail.com>
2022-03-30 18:51:50 -0300

    cursos realizados

--- Curso-Vue-js-2-O-Guia-Completo/projeto-06-base-de-conhecimento/versao-inicial/backend/config/routes.js
+++ Curso-Vue-js-2-O-Guia-Completo/projeto-06-base-de-conhecimento/versao-inicial/backend/config/routes.js
@@ -1,4 +1,8 @@
 module.exports = app =>{
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
@@ -18,4 +22,16 @@ module.exports = app =>{
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
+    
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }

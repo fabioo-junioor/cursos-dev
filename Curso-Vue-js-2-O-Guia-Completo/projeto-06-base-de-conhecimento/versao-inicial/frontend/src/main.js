b7e354fd5a60dc2e18a7f9b38a08a95b4ccd98aa
FabioJr <fj_011@hotmail.com>
2022-04-02 21:41:31 -0300

    cursos realizados

--- Curso-Vue-js-2-O-Guia-Completo/projeto-06-base-de-conhecimento/versao-inicial/frontend/src/main.js
+++ Curso-Vue-js-2-O-Guia-Completo/projeto-06-base-de-conhecimento/versao-inicial/frontend/src/main.js
@@ -2,12 +2,22 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
-import store from './config/store'
+
 import './config/bootstrap'
+import './config/msg'
+
+import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+
+//temporario
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZhYmlvIEp1bmlvciIsImVtYWlsIjoiZmpfMDExQGhvdG1haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0ODk0MzM2MiwiZXhwIjoxNjQ5MjAyNTYyfQ.51KFD9rdu-svJvGZST720MybCbj870ie6kSvXiG2okQ'
+
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
